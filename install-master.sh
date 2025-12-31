0f2507cb170d15c91181335caf2d51fc8bc34ddf
Baltharus <baltharus@gmail.com>
2025-12-31 12:43:35 -0600

    initial commit

---
+++ install-master.sh
@@ -0,0 +1,10 @@
+#1/bin/sh
+cp ./dotfiles/hyprland/hyprland.conf ~/.config/hypr/
+
+./programs/install-yay.sh
+
+./programs/install-neovim.sh
+
+./programs/install-waybar.sh
+
+./programs/install-rofi.sh
